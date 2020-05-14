fe45415338715bd3b224e8339c19943e9e275dc4
GowthamSk1 <gxs180007@utdallas.edu>
2020-05-14 17:50:55 -0500

    working_changes

--- thirtyday/lib/main.dart
+++ thirtyday/lib/main.dart
@@ -2,36 +2,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(ThirtyDayApp());
 
 class ThirtyDayApp extends StatelessWidget {
-
   final PageController _page = PageController(initialPage: 0);
-  static final IconButton _settings = IconButton(icon: const Icon(Icons.settings),
-    tooltip: 'Open settings route',
-    onPressed: (){
-    },
-  );
-  final List<Widget> appBarButtons = [_settings,];
-  static final IconButton _home = IconButton(icon: const Icon(Icons.home), iconSize: 30.0, padding: EdgeInsets.only(left: 28.0),
-    tooltip: 'Go to home',
-    onPressed: (){
-    },
-  );
-  static final IconButton _share = IconButton(icon: const Icon(Icons.share), iconSize: 30.0, padding: EdgeInsets.only(right: 28.0),
-    tooltip: 'Share with community',
-    onPressed: (){
-    },  
-  );
-  static final IconButton _profile = IconButton(icon: const Icon(Icons.face), iconSize: 30.0, padding: EdgeInsets.only(left: 28.0),
-    tooltip: 'profile',
-    onPressed: (){
-    },  
-  );
-  static final IconButton _calendar = IconButton(icon: const Icon(Icons.calendar_today), iconSize: 30.0, padding: EdgeInsets.only(right: 28.0),
-    tooltip: 'Share with community',
-    onPressed: (){
-    },  
-  );
-  final List<Widget> bottomAppBarButtons = [_home, _share, _profile, _calendar];
-
   @override
   Widget build(BuildContext context) {
     var mainApp = MaterialApp(
@@ -39,7 +10,17 @@ class ThirtyDayApp extends StatelessWidget {
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         appBar: AppBar(
           title: Text('30Day'),
-          actions: appBarButtons,
+          centerTitle: true,
+          leading: IconButton(icon: Icon(Icons.menu), onPressed: (){
+            print("Clicked");
+          },color: Colors.black,),
+          elevation: 10,
+          actions: <Widget>[
+            IconButton(icon: const Icon(Icons.settings) , color: Colors.black,
+            tooltip: 'Open settings route',
+            onPressed: (){
+            },)
+          ],
         ),
         bottomNavigationBar: BottomAppBar(
           color: Colors.blue, 
@@ -49,10 +30,28 @@ class ThirtyDayApp extends StatelessWidget {
             child: Row(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: bottomAppBarButtons,
+              //children: bottomAppBarButtons,
+              children: <Widget>[
+                  IconButton(icon: const Icon(Icons.home), iconSize: 30.0, padding: EdgeInsets.only(left: 28.0),
+                  tooltip: 'Go to home',
+                  onPressed: (){},
+                  ),
+                  IconButton(icon: const Icon(Icons.share), iconSize: 30.0, padding: EdgeInsets.only(right: 28.0),
+                  tooltip: 'Go to home',
+                  onPressed: (){},
+                  ),
+                  IconButton(icon: const Icon(Icons.face), iconSize: 30.0, padding: EdgeInsets.only(left: 28.0),
+                  tooltip: 'Go to home',
+                  onPressed: (){},
+                  ),
+                  IconButton(icon: const Icon(Icons.calendar_today), iconSize: 30.0, padding: EdgeInsets.only(right: 28.0),
+                  tooltip: 'Go to home',
+                  onPressed: (){},
+                  ),
+              ],
             ),
           )
-        
+
         ),
         body: PageView(
           controller: _page,
@@ -65,6 +64,7 @@ class ThirtyDayApp extends StatelessWidget {
                 child: Text('Empty Body 0'),
               ),
             ),
+            /*
             Center(
               child: Container(
                 child: Text('Empty Body 1'),
@@ -80,6 +80,7 @@ class ThirtyDayApp extends StatelessWidget {
                 child: Text('Empty Body 3'),
               ),
             )
+            */
           ],
         ),
         floatingActionButton: Container(
@@ -98,24 +99,3 @@ class ThirtyDayApp extends StatelessWidget {
     return mainApp;
   }
 }
-
-
-
-
-/*
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('30Day'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('start a new challenge'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('+'),
-    ),
-  )
-));
-*/
-
