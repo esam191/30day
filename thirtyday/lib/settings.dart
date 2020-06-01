b157840cf4d2535a7c1a970a661a837d36e38430
Esam Uddin <60678042+esam191@users.noreply.github.com>
2020-06-01 16:00:55 -0400

    notification_settings

--- thirtyday/lib/settings.dart
+++ thirtyday/lib/settings.dart
@@ -12,18 +12,19 @@ class AppSettings extends StatelessWidget {
         title: Text('Settings', style: TextStyle(color: Colors.black),),
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(22.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Card(
               elevation: 4.0,
-              margin: const EdgeInsets.fromLTRB(26.0, 20.0, 26.0, 20.0),
+             margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
               child: Column(
                 children: <Widget>[
                   ListTile(
                     leading: Icon(Icons.lock_outline, color: Colors.blueAccent,),
-                    title: Text("Change Password"),
+                    title: Text("Change Username"),
                     trailing: Icon(Icons.keyboard_arrow_right),
                     onTap: (){},
                   ),
@@ -34,14 +35,39 @@ class AppSettings extends StatelessWidget {
                     onTap: (){},
                   ),
                   ListTile(
-                    leading: Icon(Icons.location_on, color: Colors.blueAccent,),
-                    title: Text("Change Location"),
+                    leading: Icon(Icons.date_range, color: Colors.blueAccent,),
+                    title: Text("Change Date Format"),
                     trailing: Icon(Icons.keyboard_arrow_right),
                     onTap: (){},
                   ),
                 ],
               ),
-            )
+            ),
+            const SizedBox(height: 10.0),
+            Text('Notification Settings', style: TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold,
+              color: Colors.black54,
+            ),),
+            SwitchListTile(
+              value: true,
+              title: Text("Task Reminders"),
+              onChanged: (val){},
+            ),
+            SwitchListTile(
+              value: true,
+              title: Text("Sounds"),
+              onChanged: (val){},
+            ),
+            SwitchListTile(
+              value: false,
+              title: Text("Newsletters"),
+              onChanged: null,
+            ),
+            SwitchListTile(
+              value: true,
+              title: Text("App Updates"),
+              onChanged: (val){},
+            ),
           ],
 
         ),
