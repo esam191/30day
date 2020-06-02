d4c2db9949b467e7b28a9ae35763af9e060bbb23
Esam Uddin <60678042+esam191@users.noreply.github.com>
2020-06-01 20:57:23 -0400

    signup edit

--- thirtyday/lib/signup.dart
+++ thirtyday/lib/signup.dart
@@ -4,13 +4,16 @@ class SignUp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.green[200],
+        backgroundColor: Colors.blueAccent,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Text("Start tracking.", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 40.0, color: Colors.white),),
+              Container(
+                padding:EdgeInsets.only(right: 80.0),
+                child: Text("Start tracking.", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 40.0, color: Colors.white),),
+              ),
               Container(
                 padding: EdgeInsets.all(10.0),
                 child: Column(
@@ -25,21 +28,33 @@ class SignUp extends StatelessWidget{
                           )
                       ),
                     ),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                          )
+                      ),
+                    ),
                     SizedBox(height: 20.0),
                     Container(
-                        height: 30.0,
-                        child: Material(
-                          borderRadius: BorderRadius.circular(10.0),
+                      height: 50.0,
+                      child: Material(
+                          borderRadius: BorderRadius.circular(30.0),
                           shadowColor: Colors.black12,
-                          color: Colors.green,
+                          color: Colors.white,
                           child: GestureDetector(
                             onTap: () { Navigator.of(context).pushNamed("/SecondPage");},
                             child: Center(
-                              child: Text('SIGN UP'),
-                            ) ,
-                          ),
-                        )
-                    )
+
+                              child: Text('SIGN UP', style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold, fontSize: 16),
+                              ) ,
+                            ),
+                          )
+                      ),
+                    ),
                   ],
                 ),
               ),
